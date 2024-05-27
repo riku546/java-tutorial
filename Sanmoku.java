a41b5ade5b25afc255d8f22511127588f17fd171
riku546 <91833521+riku546@users.noreply.github.com>
2024-05-27 17:35:12 +0900

    Add files via upload

--- Sanmoku.java
+++ Sanmoku.java
@@ -9,13 +9,24 @@ public class Sanmoku {
         Scanner scanner = new Scanner(System.in);
         print_board();
         print_turn();
+        System.out.println("横,縦の数字を入力してください(例：１ 1)");
 
-        for (int i = 0; i < 9; i++) {
+
+        while (!judge_victory()) {
+
             int[] input_nums = input_num(scanner);
+            if (board[input_nums[0]][input_nums[1]] != 0) {
+                System.out.println("やり直してください");
+                continue;
+            }
             board[input_nums[0]][input_nums[1]] = turn;
             print_board();
             print_turn();
         }
+
+        System.out.println("fin");
+
+
 
         scanner.close();
 
@@ -50,13 +61,10 @@ public class Sanmoku {
         System.out.println("-------------");
         turn = 3 - turn;
 
-
-
     }
 
     public static void print_turn() {
         System.out.println("turn:" + turn);
-        // turn = 3 - turn;
     }
 
     public static int[] input_num(Scanner scanner) {
@@ -67,6 +75,35 @@ public class Sanmoku {
         nums[1] = col - 1;
 
         return nums;
+    }
+
+    public static boolean judge_victory() {
+
+
+        // 横と縦の３列
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == board[i][1] && board[i][0] == board[i][2] && board[i][0] != 0) {
+                return true;
+            }
+
+            if (board[0][i] == board[1][i] && board[0][i] == board[2][i] && board[0][i] != 0) {
+                return true;
+            }
+
+        }
+
+        // 斜め
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != 0) {
+            return true;
+        }
+
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != 0) {
+            return true;
+        }
+
+
+
+        return false;
     }
 
 }
