2ed3621225bf6be969df22e3e42b8885bab14285
riku546 <91833521+riku546@users.noreply.github.com>
2024-05-28 15:52:40 +0900

    Add files via upload

---
+++ class_tutoria2.java
@@ -0,0 +1,20 @@
+
+
+class Student {
+    public  int num = 10;
+}
+
+public class class_tutoria2{
+    public static void main(String[] args){
+        Student tarou = new Student();
+        Student hanako = new Student();
+
+        // tarou.num = 10;
+        // hanako.num = 9;
+        System.out.println(tarou.num);
+        System.out.println(hanako.num);
+    }
+}
+
+
+
