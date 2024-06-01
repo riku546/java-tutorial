58db24a2d24231add8f524fa82e7f49d0f9c2b5a
riku546 <91833521+riku546@users.noreply.github.com>
2024-06-01 18:30:28 +0900

    Add files via upload

---
+++ class_kapuseru1.java
@@ -0,0 +1,19 @@
+class New {
+    private int num;
+
+    public void setNum(int x){
+        this.num = x;
+        System.out.println(this.num);
+    }
+}
+
+
+class class_kapuseru1 {
+    public static void main(String[] args) {
+        New obj = new New();
+        obj.setNum(10);
+    }
+
+
+    
+}
