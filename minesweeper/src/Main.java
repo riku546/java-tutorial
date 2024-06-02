163c5c9379716b5b64253c8e495224749b51eba2
riku546 <91833521+riku546@users.noreply.github.com>
2024-06-02 14:28:50 +0900

    Add files via upload

---
+++ minesweeper/src/Main.java
@@ -0,0 +1,101 @@
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static int[][] directions = {
+            { -1, -1 }, // 左下
+            { 0, -1 }, // 下
+            { 1, -1 }, // 右下
+            { 1, 0 }, // 右
+            { 1, 1 }, // 右上
+            { 0, 1 }, // 上
+            { -1, 1 }, // 左上
+            { -1, 0 } // 左
+    };
+
+    public static int[][] Bomb_board = {
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    };
+
+    public static int[][] User_inputs = {
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+    };
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        display_board();
+
+        int input_row = scanner.nextInt();
+        int input_cell = scanner.nextInt();
+        put_bomb(input_row, input_cell);
+        display_board();
+
+        scanner.close();
+    }
+
+    public static void display_board() {
+        System.out.println("───────────────────────────────────────────────────────");
+        for (short i = 0; i < User_inputs.length; i++) {
+            for (short x = 0; x < User_inputs.length; x++) {
+                System.out.print("┃  ");
+                if (User_inputs[i][x] == 0) {
+                    System.out.print("-  ");
+                } else if (User_inputs[i][x] == 11) {
+                    System.out.print("💣️ ");
+                } else {
+                    System.out.print(User_inputs[i][x] + "  ");
+                }
+            }
+            System.out.print("┃");
+            System.out.println();
+            for (short y = 0; y < User_inputs.length; y++) {
+                System.out.print("┃     ");
+            }
+            System.out.print("┃");
+            System.out.println();
+        }
+        System.out.println("───────────────────────────────────────────────────────");
+
+    }
+
+    public static void put_bomb(int input_row, int input_cell) {
+        int count = 0;
+        Random rand = new Random();
+
+        while (count < 10) {
+
+            int row_index = rand.nextInt(8);
+            int cell_index = rand.nextInt(8);
+            if (row_index != input_row - 1 && cell_index != input_cell - 1 && Bomb_board[row_index][cell_index] != 11) {
+                Bomb_board[row_index][cell_index] = 11;
+                count++;
+
+            }
+        }
+
+    }
+
+    public static void count_bomb() {
+
+    }
+
+}
