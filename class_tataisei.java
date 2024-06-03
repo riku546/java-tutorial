8712358c673894b463ada1d769aa8eab67587151
riku546 <91833521+riku546@users.noreply.github.com>
2024-06-03 10:58:17 +0900

    Add files via upload

---
+++ class_tataisei.java
@@ -0,0 +1,15 @@
+public class class_tataisei {
+  
+  public static void nibai (int x ){
+    System.out.println( x * 2);
+  }
+
+  public static void nibai(double x){
+    System.out.println(x * 2);
+  }
+  
+  public static void main(String[] args) {
+    nibai(1.45);
+    nibai(20);
+  }
+}
